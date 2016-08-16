c9fdd28aaddb7709d4e9780e51e779322619f1fa
pchmelar <info@pchmelar.cz>
2016-08-16 10:49:04 +0930

    basic post component implemented

---
+++ src/components/Post.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const outerDivStyles = {
+	margin: '30px 0px 0px 0px',
+	padding: '0px 20px 10px 20px',
+	border: '1px solid black'
+}
+
+const pStyles = {
+	fontSize: '1.3em',
+	lineHeight: '1.5', 
+  textAlign: 'justify'
+}
+
+const locationStyles = {
+	padding: '15px 0px 0px 0px',
+	textAlign: 'right'
+}
+
+const Post = (props) => (
+	<Row>
+		<Col sm={10} smOffset={2}>
+			<div style={outerDivStyles}>
+				<Row>
+					<Col sm={6}>
+						<h2>{props.post.headline}</h2>
+					</Col>
+					<Col sm={6}>
+						<h4 style={locationStyles}>{props.post.location}</h4>
+					</Col>
+				</Row>
+				<br />
+				<p style={pStyles}>
+					{props.post.content}
+				</p>
+			</div>
+		</Col>
+	</Row>
+)
+
+Post.propTypes = {
+  post: React.PropTypes.object.isRequired
+}
+
+export default Post;
--- src/views/Timeline.js
+++ src/views/Timeline.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Post from '../components/Post';
+
+// obtain posts from API
+const posts = [
+	{
+		id: 0,
+		headline: "#Post2",
+		location: "Adelaide",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed lacus eget turpis lobortis dignissim. Morbi venenatis neque ut dui cursus, vitae feugiat velit efficitur. Maecenas scelerisque nisl eu pretium semper. In ultrices at nulla eu rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus bibendum augue vitae bibendum. Curabitur tempus at ipsum nec consequat. Nullam feugiat commodo arcu, in sodales lacus maximus sit amet. Phasellus posuere tortor eu metus rhoncus blandit. Nulla eu lectus iaculis, congue metus eu, fringilla ipsum. Mauris eros metus, accumsan malesuada bibendum vitae, pellentesque in lorem. Fusce enim dolor, consectetur at massa non, tempus pellentesque dolor. Quisque non suscipit massa. Phasellus lectus ante, tempus sed massa id, venenatis interdum ante. Pellentesque suscipit pellentesque metus, tempor placerat massa tempus vel. Donec non odio at ante scelerisque vestibulum ac sed dui. Vivamus facilisis mi pretium, suscipit lectus sit amet, rutrum dolor. Cras finibus sem eu libero rutrum sollicitudin. Proin nulla dolor, auctor sit amet sem sit amet, feugiat placerat nisi. Integer velit risus, faucibus eget pretium vel, sollicitudin ac libero. Morbi iaculis auctor eros in scelerisque. Nulla facilisi. Cras placerat enim mollis, pharetra est eget, pretium mauris."
+	},
+	{
+		id: 1,
+		headline: "#Post1",
+		location: "Perth",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed lacus eget turpis lobortis dignissim. Morbi venenatis neque ut dui cursus, vitae feugiat velit efficitur. Maecenas scelerisque nisl eu pretium semper. In ultrices at nulla eu rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus bibendum augue vitae bibendum. Curabitur tempus at ipsum nec consequat. Nullam feugiat commodo arcu, in sodales lacus maximus sit amet. Phasellus posuere tortor eu metus rhoncus blandit. Nulla eu lectus iaculis, congue metus eu, fringilla ipsum. Mauris eros metus, accumsan malesuada bibendum vitae, pellentesque in lorem. Fusce enim dolor, consectetur at massa non, tempus pellentesque dolor. Quisque non suscipit massa. Phasellus lectus ante, tempus sed massa id, venenatis interdum ante. Pellentesque suscipit pellentesque metus, tempor placerat massa tempus vel. Donec non odio at ante scelerisque vestibulum ac sed dui. Vivamus facilisis mi pretium, suscipit lectus sit amet, rutrum dolor. Cras finibus sem eu libero rutrum sollicitudin. Proin nulla dolor, auctor sit amet sem sit amet, feugiat placerat nisi. Integer velit risus, faucibus eget pretium vel, sollicitudin ac libero. Morbi iaculis auctor eros in scelerisque. Nulla facilisi. Cras placerat enim mollis, pharetra est eget, pretium mauris."
+	},
+];
 
 class Timeline extends Component {
   render() {
     return (
       <div>
-        <h2>Timeline</h2>
+        <Row>
+          <Col sm={12} smOffset={0} md={10} mdOffset={1} lg={8} lgOffset={2}>
+    				{posts.map(function(object, i){
+        			return <Post post={object} key={i} />;
+    				})}  
+					</Col>
+        </Row>
+        <br />
+        <br />
+        <br />
       </div>
     );
   }
