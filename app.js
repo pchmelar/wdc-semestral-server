a563e10300c557160f9f91b07ddf9f397d283230
pchmelar <info@pchmelar.cz>
2016-10-19 13:27:00 +1030

    force HTTPS on Heroku

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost/wdc');
 
+// force HTTPS on Heroku
+let forceSsl = function (req, res, next) {
+   if (req.headers['x-forwarded-proto'] !== 'https') {
+       return res.redirect(['https://', req.get('Host'), req.url].join(''));
+   }
+   return next();
+};
+if (process.env.NODE_ENV === 'production') {
+  app.use(forceSsl);
+}
+
 // CORS headers
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
